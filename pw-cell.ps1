ce9ebd994ad62b2add884566524ac7f8450adc6e
vanthang240803 <nguyenthang240803@gmail.com>
2025-03-29 20:30:18 +0700

    Add pwcell and zsh config

---
+++ pw-cell.ps1
@@ -0,0 +1,7 @@
+oh-my-posh init pwsh --config 'C:\Users\Admin\AppData\Local\Programs\oh-my-posh\themes\dracula.omp.json' | Invoke-Expression
+New-Alias -Name p -Value pnpm
+New-Alias -Name y -Value yarn
+New-Alias -Name b -Value bun
+
+
+
